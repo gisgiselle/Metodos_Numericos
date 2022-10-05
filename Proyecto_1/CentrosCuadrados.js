8ba3799d271ced7ae7dfb4a60772d112adae143e
gisgiselle <brengimolina@gmail.com>
2022-10-05 15:01:34 -0500

    Documentacion centros cuadrados

--- Proyecto_1/CentrosCuadrados.js
+++ Proyecto_1/CentrosCuadrados.js
@@ -19,8 +19,17 @@ class CentrosCuadrados {
      */
 
     squareCenterMethod(seed){
+        /*diccionario que recibe como llave el número aleatorio
+        funciona para identificar en qué momento se repite un número aleatorio
+        y así detener el loop v*/
         var randomNumbersDict = {}
 
+        /*Recibe una semilla y la eleva al cuadrado, obteniendo un número
+         de 8 dígitos. Se le quitan 2 números de la izquierda y 2 de la derecha.
+         El número reducido es la nueva semilla. 
+         Si el número aleatorio obtenido ya se encuentra en el diccionario, 
+         se le asigna el valor de 2 y se termina el loop
+         */
         while (randomNumbersDict[seed] == 1 || randomNumbersDict[seed] == null){   
             var generator = Math.pow(seed, 2)
 
@@ -62,5 +71,5 @@ class CentrosCuadrados {
 }
 
 
-const obj = new CentrosCuadrados(3547)
-obj.squareCenterMethod(3547)
+//const obj = new CentrosCuadrados(3547)
+//obj.squareCenterMethod(3547)
