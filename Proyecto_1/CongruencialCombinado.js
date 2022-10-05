487bdb86cf2c63764c9475fe6fed81856dcd2d50
gisgiselle <brengimolina@gmail.com>
2022-10-05 18:27:41 -0500

    documentacion Congruencial combinado y multiplicativo

--- Proyecto_1/CongruencialCombinado.js
+++ Proyecto_1/CongruencialCombinado.js
@@ -1,66 +1,56 @@
 /** Clase que genera números aleatorios utilizando 
  * el método congruencial combinado.
   */
-function congruencialMultiplicativo(x, a, mod){
-    period = 0
-    flag = 0
-    ri = 0
-    sum = 0
-    arr = []
-
-
-    while (flag != x){
-        if(flag == 0){
-            flag = x
-        }
-        x = (a *x ) % mod
-        console.log('x: '+x) 
-        arr.push(x)
-        //console.log('array '+arr)
-        period += period
-
-        ri = x/mod
-        console.log('Ri: '+ri) 
-
-    }
-    return arr
-
-}
-
-function conguencialCombinadoMalo(x,y,ax,ay,modx, mody,k){
-    sum = 0
-    //A===B (mod C)
-    //A mod C = B mod C
-    for(var j = 1; j < k; j++){
-        var xn = congruencialMultiplicativo(x,ax,modx)
-        var yn = congruencialMultiplicativo(y,ay,mody)
-        console.log('xn: '+xn[j-1])
-        console.log('yn: '+yn[j-1])
-
-        console.log('----- ajkwr '+wn)
-        sum += ((-1)**(j-1))(xn)
-    }
-}
-
-function congruencialCombinado(x,a,mod,k){
-    var yi = 0
-    var sum = 0
-    var zi = 0
+class CongruencialCombinado {
+//Se requiere añadir funcionalidad para exportar el metodo de la clase congruencial multiplicativo
+    congruencialMultiplicativo(x, a, mod){
+        var period = 0
+        var flag = 0
+        var ri = 0
+        var sum = 0
+        var arr = []
     
-    for(var j = 0; j < k; j++) {
-        console.log('X EN COMBINADO ES '+x)
-        yi = congruencialMultiplicativo(x,a,mod)
-        yi[j] = yi[j]*(-1)**(j)
-
-        console.log('sumanding '+sum + ' '+yi[j])
-
-        sum += yi[j]
+        while (flag != x){
+            if(flag == 0){
+                flag = x
+            }
+            x = (a *x ) % mod
+            arr.push(x)
+            period += period
+    
+            ri = x/mod
+    
+        }
+        return arr
     }
-    mod--
-    zi = sum %(mod)
-    console.log('zi: '+zi)
 
+    /**
+     * 
+     * @param {*} x 
+     * @param {*} a 
+     * @param {*} mod 
+     * @param {*} k 
+     * @returns 
+     */
+    congruencialCombinado(x,a,mod,k){
+        var yi = 0
+        var sum = 0
+        var zi = 0
+        
+        for(var j = 0; j < k; j++) {
+            yi = this.congruencialMultiplicativo(x,a,mod)
+            yi[j] = yi[j]*(-1)**(j)
+
+            sum += yi[j]
+        }
+        mod--
+        zi = sum %(mod)
+        return sum, zi
+
+        }
 }
+const obj = new CongruencialCombinado()
+obj.congruencialCombinado(15,35,64,2)
 
-congruencialCombinado(15,35,64,2)
+
 
--- Proyecto_1/CongruencialMultiplicativo.js
+++ Proyecto_1/CongruencialMultiplicativo.js
@@ -1,26 +1,42 @@
-function congruencialMultiplicativo(x, a, mod){
-    period = 0
-    flag = 0
-    ri = 0
-    sum = 0
-    arr = []
+/** Clase para generar números aleatorios entre 0 y 1.
+ * 
+ * @class
+ * Utiliza el método congruencial multiplicativo*/
+const { Module } = require("module")
 
-    console.log('old x '+x);
-    while (flag != x){
-        if(flag == 0){
-            flag = x
+  
+
+class CongruencialMultiplicativo {
+    /** Realiza el cálculo de números aleatorios 
+     * utilizando la fórmula xi+1 = (a * xi)mod m
+     * @param {int} x - Valor inicial o semilla
+     * @param {int} a - Multiplicador
+     * @param {int} mod - Módulo
+     */
+    congruencialMultiplicativo(x, a, mod){
+        var period = 0
+        var flag = 0
+        var ri = 0
+        var sum = 0
+        var arr = []
+    
+        while (flag != x){
+            if(flag == 0){
+                flag = x
+            }
+            x = (a *x ) % mod
+            arr.push(x)
+            period += period
+    
+            ri = x/mod
+    
         }
-       
-
-        x = (a *x ) % mod
-        console.log('x: '+x) 
-        arr.push(x)
-        period += period
-
-        ri = x/mod
-        console.log('Ri: '+ri) 
-
+        return arr
     }
-
 }
 
+module.exports={
+    CongruencialMultiplicativo,
+}
+/*const obj = new CongruencialMultiplicativo()
+obj.congruencialMultiplicativo(4,5,8)*/
